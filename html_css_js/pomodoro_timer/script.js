caa028cec157a4cdb0998966738352b80ef639b8
Nimesh-Nagar <n.nimesh02@gmail.com>
2024-05-01 19:09:52 +0530

    Pomodoro Timer

---
+++ html_css_js/pomodoro_timer/script.js
@@ -0,0 +1,46 @@
+const startEl = document.getElementById("start")
+const pauseEl = document.getElementById("pause")
+const resetEl = document.getElementById("reset")
+const timerEl = document.getElementById("timer")
+
+let interval;
+let timeLeft = 10;  // Start Time for pomodoro 
+
+function updateTimer(){
+    let minutes = Math.floor(timeLeft / 60);
+    let seconds = timeLeft % 60;
+    let formattedTime = `${minutes.toString().padStart(2, "0")}:${seconds.toString().padStart(2, "0")}`
+
+    timerEl.innerHTML = formattedTime
+}
+
+function startTimer(){
+    interval = setInterval(() => {
+        timeLeft--; 
+        updateTimer();
+
+        if (timeLeft === 0){
+            clearInterval(interval);
+            alert("Time's Up !");
+            timeLeft = 1500;
+            updateTimer();
+        }
+
+    }, 1000);
+}
+
+function pauseTimer(){
+    clearInterval(interval);
+}
+
+function resetTimer(){
+    clearInterval(interval);
+    timeLeft = 1500;
+    updateTimer();
+}
+
+startEl.addEventListener("click", startTimer)
+pauseEl.addEventListener("click", pauseTimer)
+resetEl.addEventListener("click", resetTimer)
+
+
