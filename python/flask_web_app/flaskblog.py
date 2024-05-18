433b45a3a83aa0ec57ab9c9b8c23e97b78125074
Nimesh-Nagar <n.nimesh02@gmail.com>
2024-05-19 02:05:07 +0530

    web app using flask

---
+++ python/flask_web_app/flaskblog.py
@@ -0,0 +1,31 @@
+from flask import Flask , render_template, url_for
+app = Flask(__name__)
+
+posts = [
+    {
+        "author" : "Nimesh Nagar",
+        "title" : "Blog Post 1",
+        "content" : "my first post",
+        "date_posted" : "May 18 , 2024"
+    },
+    {
+        "author" : "Mahendra Nagar",
+        "title" : "Blog Post 2",
+        "content" : "my second post",
+        "date_posted" : "May 19 , 2024"
+    }
+
+]
+
+@app.route("/")
+@app.route("/home")
+def home():
+    return render_template('home.html', posts=posts)
+
+@app.route("/about")
+def about():
+    return render_template('about.html', title="about")
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
