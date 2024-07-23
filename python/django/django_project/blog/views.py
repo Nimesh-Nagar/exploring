ade9167bf98fcd4a824e8ef97b5971e3e0932a10
Nimesh-Nagar <n.nimesh02@gmail.com>
2024-07-23 23:09:50 +0530

    adding post using admin page

--- python/django/django_project/blog/views.py
+++ python/django/django_project/blog/views.py
@@ -1,28 +1,29 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+# from django.http import HttpResponse
+from .models import Post
 
 # dummay data 
 
-posts = [
-    {
-        'author': 'Nimesh',
-        'title' : 'Blog Post 1',
-        'content' : 'First post contents',
-        'date_posted': 'July 21, 2024'
-    },
-    {
-        'author': 'Abhishek',
-        'title' : 'Blog Post 2',
-        'content' : 'Second post contents',
-        'date_posted': 'July 25, 2024'
-    }
-]
+# posts = [
+#     {
+#         'author': 'Nimesh',
+#         'title' : 'Blog Post 1',
+#         'content' : 'First post contents',
+#         'date_posted': 'July 21, 2024'
+#     },
+#     {
+#         'author': 'Abhishek',
+#         'title' : 'Blog Post 2',
+#         'content' : 'Second post contents',
+#         'date_posted': 'July 25, 2024'
+#     }
+# ]
 
 
 # home page of blog website
 def home(request):
     context = {
-        'posts': posts
+        'posts': Post.objects.all()
     }
 
     # return HttpResponse('<h1>Blog Home </h1>')
