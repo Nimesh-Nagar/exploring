c9800e9556b442dcb44d351baf0ae8960d9d52c7
Nimesh-Nagar <n.nimesh02@gmail.com>
2024-07-25 18:56:50 +0530

    login logout feature

--- python/django/django_project/users/views.py
+++ python/django/django_project/users/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render, redirect 
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib import messages 
+from django.contrib.auth.decorators import login_required
+
+from django.contrib.auth import logout
+from django.urls import reverse
 
 
 """
@@ -18,10 +22,23 @@ def register(request):
         if form.is_valid():
             form.save()
             username = form.cleaned_data.get('username')
-            messages.success(request, f"Account created for {username} !")
-            return redirect('blog-home')
+            messages.success(request, f"Your account with usename : {username} has been created ! \n You are now able to login")
+            return redirect('login')
     else:
         form = UserCreationForm()
 
     return render(request, 'users/register.html', {'form': form})
+
+
+
+
+def custom_logout_view(request):
+    logout(request)
+    # Redirect to a success page or home page
+    return redirect(reverse('login'))  # or use 'login' if you want to redirect to the login page
+
+@login_required
+def profile(request):
+    return render(request, 'users/profile.html')
+
 
