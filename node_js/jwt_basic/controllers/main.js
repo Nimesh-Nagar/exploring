e4e68a72c88b275a746679071d63a188cee2e80c
Nimesh-Nagar <n.nimesh02@gmail.com>
2024-12-22 14:25:39 +0530

    autherization

--- node_js/jwt_basic/controllers/main.js
+++ node_js/jwt_basic/controllers/main.js
@@ -24,7 +24,7 @@ const login = (req,res) => {
     const token = jwt.sign({ id, username }, process.env.JWT_SECRET, {
         expiresIn: '30d',
     })
-
+    
     res.status(200).json({ msg: 'user created', token})
 
 }
